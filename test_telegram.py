31a428e604715faa3491826a5ad8c88af3f3b8ab
Gitesh Kumar <105482735+gitesh-kumar@users.noreply.github.com>
2025-09-04 17:05:22 +0200

    Update test_telegram.py

--- test_telegram.py
+++ test_telegram.py
@@ -1,8 +1,12 @@
 import os
+import asyncio
 from telegram import Bot
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 bot = Bot(token=TOKEN)
 
-bot.send_message(chat_id=CHAT_ID, text="✅ GitHub Actions Telegram test successful!")
+async def send_test_message():
+    await bot.send_message(chat_id=CHAT_ID, text="✅ GitHub Actions Telegram test successful!")
+
+asyncio.run(send_test_message())
